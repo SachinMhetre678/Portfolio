0f286893c66c8b24e410b13831d40105326284e6
Sachin_Mhetre <138901374+SachinMhetre678@users.noreply.github.com>
2024-06-05 13:51:45 +0530

    commit

--- app.js
+++ app.js
@@ -6,27 +6,29 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// Set the view engine to HTML
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     const images = '/images/profile.jpg'; // Adjusted to use the path served by express.static
-    res.render('index', { images: images });
+    res.render('index.html', { images: images });
 });
 
 app.get("/about", (req, res) => {
-    res.render('about');
+    res.render('about.html');
 });
 
 app.get("/project", (req, res) => {
-    res.render('project');
+    res.render('project.html');
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact");
+    res.render("contact.html");
 });
 
 app.listen(PORT, () => {
