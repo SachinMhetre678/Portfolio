7c4bc2e3e9ca86073b8d5152fa1e43ba6dc60182
Sachin_Mhetre <138901374+SachinMhetre678@users.noreply.github.com>
2024-06-05 17:05:00 +0530

    cmmoit

--- index.js
+++ index.js
@@ -2,35 +2,29 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-require("dotenv").config();
-
-const PORT = process.env.PORT;
-
-// Set the view engine to HTML
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     const images = '/images/profile.jpg'; // Adjusted to use the path served by express.static
-    res.render('index.html');
+    res.render('index', { images: images });
 });
 
 app.get("/about", (req, res) => {
-    res.render('about.html');
+    res.render('about');
 });
 
 app.get("/project", (req, res) => {
-    res.render('project.html');
+    res.render('project');
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact.html");
+    res.render("contact");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+app.listen(8080, () => {
+    console.log("Server is listening on port 8080");
 });
